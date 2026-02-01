973edcba08bae4bf758bc9354f9e78bbbe5f9908
Ravi Kant Jha <ravirbikant@gmail.com>
2026-02-01 13:43:40 +0530

    Add logout functionality

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,20 @@
 import { useNavigate, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import axios from "axios";
 import { removeUser } from "./store/userSlice.js";
+import { BASE_URL } from "./constants.js";
 
 function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  function handleLogout() {
-    dispatch(removeUser());
-    navigate("/");
+  async function handleLogout() {
+    try {
+      await axios.post(`${BASE_URL}/logout`, null, { withCredentials: true });
+    } finally {
+      dispatch(removeUser());
+      navigate("/");
+    }
   }
 
   return (
@@ -19,7 +25,11 @@ function Navbar() {
         </Link>
       </div>
       <div className="navbar-end">
-        <button type="button" className="btn btn-ghost btn-sm" onClick={handleLogout}>
+        <button
+          type="button"
+          className="btn btn-ghost btn-sm"
+          onClick={handleLogout}
+        >
           Logout
         </button>
       </div>
